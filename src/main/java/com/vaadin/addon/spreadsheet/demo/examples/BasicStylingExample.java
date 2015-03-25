7d20a05a9a762f1a335e99d98d03a4f484a7fffd
Guillermo Alvarez <guillermo@vaadin.com>
2015-03-25 13:17:08 +0200

    Update colopicker color when selection changes (#16889)
    Change-Id: I531265fa10e2fd2c0aa2ab854e707d6d2c59d9a7

--- src/main/java/com/vaadin/addon/spreadsheet/demo/examples/BasicStylingExample.java
+++ src/main/java/com/vaadin/addon/spreadsheet/demo/examples/BasicStylingExample.java
@@ -1,5 +1,8 @@
 package com.vaadin.addon.spreadsheet.demo.examples;
 
+import static com.vaadin.shared.ui.colorpicker.Color.BLACK;
+import static com.vaadin.shared.ui.colorpicker.Color.WHITE;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +15,8 @@ import org.apache.poi.xssf.usermodel.XSSFColor;
 import org.apache.poi.xssf.usermodel.XSSFFont;
 
 import com.vaadin.addon.spreadsheet.Spreadsheet;
+import com.vaadin.addon.spreadsheet.Spreadsheet.SelectionChangeEvent;
+import com.vaadin.addon.spreadsheet.Spreadsheet.SelectionChangeListener;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.shared.ui.colorpicker.Color;
 import com.vaadin.ui.Button;
@@ -24,11 +29,16 @@ import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.components.colorpicker.ColorChangeEvent;
 import com.vaadin.ui.components.colorpicker.ColorChangeListener;
 
-public class BasicStylingExample implements SpreadsheetExample {
+public class BasicStylingExample implements SpreadsheetExample,
+        SelectionChangeListener {
+
+    private static final long serialVersionUID = 6330513476932056681L;
 
     private VerticalLayout layout;
     private HorizontalLayout stylingToolbar;
     private Spreadsheet spreadsheet;
+    private ColorPicker backgroundColor;
+    private ColorPicker fontColor;
 
     public BasicStylingExample() {
         layout = new VerticalLayout();
@@ -48,6 +58,7 @@ public class BasicStylingExample implements SpreadsheetExample {
 
     private void initSpreadsheet() {
         spreadsheet = new Spreadsheet();
+        spreadsheet.addSelectionChangeListener(this);
     }
 
     private void initStyleToolbar() {
@@ -59,7 +70,7 @@ public class BasicStylingExample implements SpreadsheetExample {
                 updateSelectedCellsBold();
             }
         });
-        ColorPicker backgroundColor = new ColorPicker();
+        backgroundColor = new ColorPicker();
         backgroundColor.setCaption("Background Color");
         backgroundColor.addColorChangeListener(new ColorChangeListener() {
             @Override
@@ -67,7 +78,7 @@ public class BasicStylingExample implements SpreadsheetExample {
                 updateSelectedCellsBackgroundColor(event.getColor());
             }
         });
-        ColorPicker fontColor = new ColorPicker();
+        fontColor = new ColorPicker();
         fontColor.setCaption("Font Color");
         fontColor.addColorChangeListener(new ColorChangeListener() {
             @Override
@@ -176,6 +187,42 @@ public class BasicStylingExample implements SpreadsheetExample {
             newXFont.setColor(originalXFont.getXSSFColor());
         }
         return newFont;
+    }
+
+    @Override
+    public void onSelectionChange(SelectionChangeEvent event) {
+        CellReference selectedCell = event.getSelectedCellReference();
+        Cell cell = spreadsheet.getCell(selectedCell.getRow(),
+                selectedCell.getCol());
+        backgroundColor.setColor(WHITE);
+        fontColor.setColor(BLACK);
+        if (cell != null) {
+            // This cast an only be done when using .xlsx files
+            XSSFCellStyle style = (XSSFCellStyle) cell.getCellStyle();
+            if (style != null) {
+                XSSFFont font = style.getFont();
+                if (font != null) {
+                    XSSFColor xssfFontColor = font.getXSSFColor();
+                    if (xssfFontColor != null) {
+                        fontColor.setColor(convertColor(xssfFontColor));
+                    }
+                }
+                XSSFColor foregroundColor = style.getFillForegroundColorColor();
+                if (foregroundColor != null) {
+                    backgroundColor.setColor(convertColor(foregroundColor));
+                }
+            }
+        }
+    }
+
+    private Color convertColor(XSSFColor foregroundColor) {
+        byte[] argb = foregroundColor.getARgb();
+        return new Color(byteToInt(argb[1]), byteToInt(argb[2]),
+                byteToInt(argb[3]), byteToInt(argb[0]));
+    }
+
+    private int byteToInt(byte byteValue) {
+        return byteValue & 0xFF;
     }
 
 }
